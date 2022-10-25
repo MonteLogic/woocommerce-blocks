5e7a571e57867297d124802d86277ae88743542e
Monte Logic <dchavours@gmail.com>
2022-10-25 12:05:33 -0500

    ProductsByAttribute runs Frontend JavaScript

---
+++ assets/js/blocks/products-by-attribute/frontend.ts
@@ -0,0 +1,32 @@
+import { renderFrontend } from '@woocommerce/base-utils';
+
+/**
+ * Internal dependencies
+ */
+ import { ProductsByAttributeBlock } from './block';
+
+
+
+/**
+ * Wrapper component to supply the notice provider.
+ *
+ * @param {*} props
+ */
+
+// const ProductsByAttributeFrontend = ( props ) =>{
+//     return <ProductsByAttributeBlock {...props}/>;
+// }
+
+// console.log(555);
+// const getProps = ( el ) => ( {
+// 	attributes: JSON.parse( el.dataset.attributes ),
+// } );
+
+// renderFrontend( {
+// 	selector: '.wp-block-woocommerce-products-by-attribute',
+// 	ProductsByAttributeBlock,
+// 	getProps,
+// } );
+
+console.log(5);
+console.log(7);
